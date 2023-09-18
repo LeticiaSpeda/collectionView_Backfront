598300c50f5017c89b935b080e2a4120bbac2cd8
Leticia Speda <leticiaspeda@gmail.com>
2023-09-18 11:47:12 -0300

    build: configuracoes iniciais

---
+++ CollectionView_Aula/CollectionView_Aula/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionView_Aula
+//
+//  Created by Leticia Speda on 18/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
